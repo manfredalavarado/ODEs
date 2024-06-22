c5cb6b51556465a354a62f1ab5e925617184270c
Manfred <Manfred.Alvarado@ucr.ac.cr>
2024-06-21 22:05:15 -0600

    Metodos de ODE

--- ODE/ODEs.py
+++ ODE/ODEs.py
@@ -1,9 +1,46 @@
-# ODE/__init__.py
+# ODE/ODEs.py
 
-"""
-Hacer calculos con tus propias funciones.
+def Euler(f,xi,ti,tf,N):
+    
+    t = np.linspace(ti,tf,N)
+    x = np.zeros(t.size)
+    x[0] = xi
+    h = t[1] - t[0]
+    
+    for i in range(t.size - 1):
+        x[i+1] = x[i] + h*f(x[i],t[i])
+    
+    return t,x
 
-Modulos exportados:
 
-    - 'ODEs': Tiene funciones para calcular integrales.
-    """
+def RK2(f,xi,ti,tf,N):
+    
+    t = np.linspace(ti,tf,N)
+    x = np.zeros(t.size)
+    x[0] = xi
+    h = t[1] - t[0]
+    
+    for i in range(t.size-1):
+        k1 = h*f(x[i],t[i])
+        k2 = h*f(x[i] + (k1/2), t[i] + (h/2))
+        n = x[i] + (h/2)*f(x[i],t[i])
+        x[i+1] = x[i] + h*f(n, t[i] + h/2)
+    
+    return x,t
+
+
+def RK4(f,xi,ti,tf,N):
+
+    t = np.linspace(ti,tf,N)
+    x = np.zeros(t.size)
+    x[0] = xi
+    h = t[1] - t[0]
+    
+    for i in range(t.size-1):
+        k1 = h*f(x[i],t[i])
+        k2 = h*f(x[i] + (k1/2), t[i] + (h/2))
+        k3 = h*f(x[i] + (k2/2), t[i] + (h/2))
+        k4 = h*f(x[i] + k3, t[i] + h)
+        x[i+1] = x[i] + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+    
+    return x,t
