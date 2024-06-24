c75e895ad3d9e03194312ee05002eeff3fc4404b
Manfred <Manfred.Alvarado@ucr.ac.cr>
2024-06-23 22:29:23 -0600

    final

--- ODE/__init__.py
+++ ODE/__init__.py
@@ -1,6 +1,6 @@
 # ODE/__init__.py
 """
-Resuelve tus ecuaciones diferenciales ordinarias mediante el uso de este paqueteel cual incluye:
+Resuelve tus ecuaciones diferenciales ordinarias mediante el uso de este paquete el cual incluye:
     - 'Euler' : contiene el metodo de euler para resolucion de ODEs.
     - 'RK2' : contiene el metodo rakuta de 2 indices para la resolucion de ODEs.
     - 'RK4' : contiene el metodo rakuta de 4 indices para la resolucion de ODEs.
